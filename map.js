b77ebca9fdfea660d8938d987f16ee1c840a7595
smithwebtek <brad@smithwebtek.com>
2019-01-19 19:43:14 -0500

    Consolidate code into map.js; add https to link for H

--- map.js
+++ map.js
@@ -4,6 +4,23 @@ $(function () {
 	newVoterForm()
 	// loadAllVoters()
 });
+let testVoters = [
+	[42.346268, -71.095764],
+	[42.380098, -71.116629],
+	[42.36306, - 71.00639],
+	[42.390270801811454, -71.10008122399451]
+]
+
+function loadTestVoters() {
+	for (let i = 0; i < testVoters.length; i++) {
+		testVoters[i] = new L.marker(testVoters[i], { icon: voteOpposeIcon }).addTo(map)
+	}
+}
+
+let addresses = [
+	'138 Portland St, Boston, MA 02114',
+	'181 Cambridge St, Boston, MA 02114'
+]
 
 // baseURL can be toggled between your local running Rails server or Heroku demo server
 const baseURL = 'https://voter-preference-api.herokuapp.com/api/'
