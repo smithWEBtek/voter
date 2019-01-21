07707fad685d70e7525d4bfb2204e422f88a1a6d
smithwebtek <brad@smithwebtek.com>
2019-01-21 06:09:23 -0500

    Move views into separate file

---
+++ views.js
@@ -0,0 +1,38 @@
+// reset the map view to original page load; connected to click event on logo-image
+function refresh() {
+	$('img#logo-image').on('click', function (event) {
+		event.preventDefault()
+		map.setView([42.358056, -71.063611], 11);
+	})
+	resetAddressForm()
+}
+
+// listen for click of button, pan out to greater Boston, zoom level 10
+function bostonView() {
+	$('#load-boston-view').on('click', function (event) {
+		event.preventDefault()
+		event.stopPropagation()
+		map.setView([42.358056, -71.063611], 11);
+		resetAddressForm()
+	})
+}
+
+// listen for click of button, pan out to greater Boston, zoom level 10
+function newEnglandView() {
+	$('#load-new-england-view').on('click', function (event) {
+		event.preventDefault()
+		event.stopPropagation()
+		map.setView([42.163686, -72.635366], 7);
+		resetAddressForm()
+	})
+}
+
+// listen for click of button, pan out to national view, zoom level 3
+function nationalView() {
+	$('#load-national-view').on('click', function (event) {
+		event.preventDefault()
+		event.stopPropagation()
+		map.setView([38.645955, -95.153003], 4);
+		resetAddressForm()
+	})
+}
