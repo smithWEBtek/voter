06c381a27c7df5efa56a8a2a54c955cb4d1d1450
Brad Smith <brad@smithwebtek.com>
2019-11-21 08:42:01 -0500

    Update comments in views.js

--- views.js
+++ views.js
@@ -1,38 +1,38 @@
-// reset the map view to original page load; connected to click event on logo-image
+// reset the map view to original page load; connected to click event on logo-image, zoom level 11
 function refresh() {
-	$('img#logo-image').on('click', function (event) {
-		event.preventDefault()
-		map.setView([42.358056, -71.063611], 11);
-	})
-	resetAddressForm()
+  $('img#logo-image').on('click', function (event) {
+    event.preventDefault()
+    map.setView([42.358056, -71.063611], 11);
+  })
+  resetAddressForm()
 }
 
-// listen for click of button, pan out to greater Boston, zoom level 10
+// listen for click of button, pan out to greater Boston, zoom level 11
 function bostonView() {
-	$('#load-boston-view').on('click', function (event) {
-		event.preventDefault()
-		event.stopPropagation()
-		map.setView([42.358056, -71.063611], 11);
-		resetAddressForm()
-	})
+  $('#load-boston-view').on('click', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    map.setView([42.358056, -71.063611], 11);
+    resetAddressForm()
+  })
 }
 
-// listen for click of button, pan out to greater Boston, zoom level 10
+// listen for click of button, pan out to greater Boston, zoom level 7
 function newEnglandView() {
-	$('#load-new-england-view').on('click', function (event) {
-		event.preventDefault()
-		event.stopPropagation()
-		map.setView([42.163686, -72.635366], 7);
-		resetAddressForm()
-	})
+  $('#load-new-england-view').on('click', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    map.setView([42.163686, -72.635366], 7);
+    resetAddressForm()
+  })
 }
 
-// listen for click of button, pan out to national view, zoom level 3
+// listen for click of button, pan out to national view, zoom level 4
 function nationalView() {
-	$('#load-national-view').on('click', function (event) {
-		event.preventDefault()
-		event.stopPropagation()
-		map.setView([38.645955, -95.153003], 4);
-		resetAddressForm()
-	})
+  $('#load-national-view').on('click', function (event) {
+    event.preventDefault()
+    event.stopPropagation()
+    map.setView([38.645955, -95.153003], 4);
+    resetAddressForm()
+  })
 }
